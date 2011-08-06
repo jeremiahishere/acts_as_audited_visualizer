6776d3a2df2ff779ca35901857728f9be5b7367b
Jeremiah Hemphill <jeremiah.hemphill@gmail.com>
2011-08-06 15:49:10 -0400

    Fixed the controller.  It now loads but has little functionality.

--- app/controllers/acts_as_audited_visualizer/visualizers_controller.rb
+++ app/controllers/acts_as_audited_visualizer/visualizers_controller.rb
@@ -1,5 +1,5 @@
 module ActsAsAuditedVisualizer
-  class VisualizersController < AppicationController
+  class VisualizersController < ApplicationController
     def index
       # canvas goes here
     end
@@ -13,17 +13,17 @@ module ActsAsAuditedVisualizer
       audits.each do |audit|
         record = []
         # user
-        record.push {
+        record.push({
           :model => "User", 
           :id => audit.user_id, 
           :name => User.find_by_id(audit.user_id).name
-        }
+        })
         # base record
-        record.push {
-          :model => audit.auditable_type
-          :id => audit.auditable_id
+        record.push({
+          :model => audit.auditable_type,
+          :id => audit.auditable_id,
           :name => audit.audtiable_type.constantize.find_by_id(audit.auditable_id).name
-        }
+        })
         # guess the other records
         audit.audit_changes.each_pair do |key, value|
           if key.match(/_id$/)
@@ -31,11 +31,11 @@ module ActsAsAuditedVisualizer
             if key_model.constantize
               instance = key_model.constantize.find_by_id(value)
               if instance
-                record.push {
+                record.push({
                   :model => key_model,
                   :id => value,
                   :name => instance.name
-                }
+                })
               end
             end
           end
