9b2dfbc04136ce842a0c6a3eb3fd35219c8b8e03
jeremiahishere <jeremiah@cloudspace.com>
2011-08-15 21:52:01 -0400

    Moved the fun code to the old_index view.  Starting on serious code now.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :acts_as_audited_visualizer do
     match "/" => "visualizers#index"
+    match "/old" => "visualizers#old_index"
     match "/update_audits/:timestamp" => "visualizers#update_audits"
   end
 end
